8b9a294467d5a98a766aaeae48c7a0995d3b040d
mathyasp <mathyastp@gmail.com>
2024-06-13 16:44:18 -0400

    Challenge 3 complete

--- scripts.js
+++ scripts.js
@@ -1,14 +1,11 @@
-// Functions 
-function getWeather(apikey, zip, callback) {
-  const apiKey = apikey
-  const units = 'imperial'
-  const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
-  fetch(path)
+function getWeather(apikey, zip) {
+  const apiKey = apikey;
+  const units = 'imperial';
+  const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`;
+  
+  return fetch(path)
     .then(res => res.json())
-    .then(json => {
-      callback(json);
-    })
-    .catch(err => console.log(err.message))
+    .catch(err => console.log(err.message));
 }
 
 module.exports = getWeather;
