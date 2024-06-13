49ee3fd9d95e3bb7990c4e9910901dc3ce2bce56
mathyasp <mathyastp@gmail.com>
2024-06-13 16:45:10 -0400

    Challenge 4 complete

--- scripts.js
+++ scripts.js
@@ -1,11 +1,15 @@
-function getWeather(apikey, zip) {
+async function getWeather(apikey, zip) {
   const apiKey = apikey;
   const units = 'imperial';
   const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`;
-  
-  return fetch(path)
-    .then(res => res.json())
-    .catch(err => console.log(err.message));
+
+  try {
+    const res = await fetch(path);
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
 module.exports = getWeather;
