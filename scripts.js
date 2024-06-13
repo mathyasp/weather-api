12f50520a45c0555db0b24d5d4ac4b2821302d30
mathyasp <mathyastp@gmail.com>
2024-06-13 16:49:07 -0400

    Challenge 5 complete

--- scripts.js
+++ scripts.js
@@ -6,7 +6,16 @@ async function getWeather(apikey, zip) {
   try {
     const res = await fetch(path);
     const data = await res.json();
-    return data;
+
+    // Create a new object with properties from the weather JSON
+    const weatherData = {
+      temperature: data.main.temp,
+      description: data.weather[0].description,
+      city: data.name,
+      country: data.sys.country
+    };
+
+    return weatherData;
   } catch (err) {
     console.log(err.message);
   }
